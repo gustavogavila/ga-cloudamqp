89fcf217c2d11ecb6cacf97ffdfbc535309f0104
Gustavo Avila <gustavogavila01@gmail.com>
2022-04-09 23:30:19 -0300

    initial commit

---
+++ src/main/java/com/gustavoavila/cloudamqp/CloudamqpApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoavila.cloudamqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudamqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudamqpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavoavila/cloudamqp/CloudamqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoavila.cloudamqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudamqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
